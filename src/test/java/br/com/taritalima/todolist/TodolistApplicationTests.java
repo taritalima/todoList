d7921fb391d89c24d9de60e6539becaef5760c50
Tarita De Lima Silva <tarita36@hotmail.com>
2023-10-15 13:15:55 -0300

    projeto todoList

---
+++ src/test/java/br/com/taritalima/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.taritalima.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
